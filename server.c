6bf1b7043da2ce6bf94f57f21ffa81b96fca9c49
aiphae <vl.diachenkoo@gmail.com>
2025-05-20 14:21:34 +0300

    Implemented directory listing

--- server.c
+++ server.c
@@ -1,13 +1,88 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <winsock2.h>
-#include <ws2tcpip.h>
 
-#pragma comment(lib, "ws2_32.lib")
+char *construct_html_page(const char *directory) {
+    const char *header_template =
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Content-Length: %d\r\n"
+            "Connection: close\r\n"
+            "\r\n";
 
-int main(int argc, char **argv) {
+    const char *html_start = "<html><body><h1>Directory Listing</h1><ul>";
+    const char *html_end = "</ul></body></html>";
+
+    int html_buffer_size = 4096;
+    char *html_body = malloc(html_buffer_size);
+    if (!html_body) {
+        return NULL;
+    }
+    strcpy(html_body, html_start);
+
+    char search_path[MAX_PATH];
+    snprintf(search_path, sizeof(search_path), "%s\\*", directory);
+
+    WIN32_FIND_DATA find_data;
+    HANDLE hFind = FindFirstFile(search_path, &find_data);
+    if (hFind != INVALID_HANDLE_VALUE) {
+        do {
+            if (!(find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)) {
+                // Generate HTML list item for the file
+                char item[1024];
+                snprintf(item,
+                         sizeof(item),
+                         "<li><a href=\"/%s\">%s</a></li>",
+                         find_data.cFileName, find_data.cFileName);
+
+                // Resize buffer if necessary
+                if (strlen(html_body) + strlen(item) + strlen(html_end) + 1 > html_buffer_size) {
+                    html_buffer_size *= 2;
+                    html_body = realloc(html_body, html_buffer_size);
+                    if (!html_body) {
+                        return NULL;
+                    }
+                }
+
+                strcat(html_body, item);
+            }
+        } while (FindNextFile(hFind, &find_data));
+
+        FindClose(hFind);
+    }
+
+    strcat(html_body, html_end);
+
+    int body_len = (int) strlen(html_body);
+    int header_len = snprintf(NULL, 0, header_template, body_len);
+    char *response = malloc(header_len + body_len + 1);
+    if (!response) {
+        free(html_body);
+        return NULL;
+    }
+
+    sprintf(response, header_template, body_len);
+    strcat(response, html_body);
+
+    free(html_body);
+
+    return response;
+}
+
+int main() {
+    char directory[256];
+    printf("Directory to share:\n");
+    if (fgets(directory, sizeof(directory), stdin) != NULL) {
+        directory[strcspn(directory, "\n")] = 0;
+    }
+    else {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
     WSADATA wsa_data;
     int result;
-
     result = WSAStartup(MAKEWORD(2, 2), &wsa_data);
     if (result != 0) {
         printf("WSAStartup() failed: %d\n", result);
@@ -21,10 +96,9 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    struct sockaddr_in address;
-    memset(&address, 0, sizeof(address));
+    struct sockaddr_in address = {0};
     address.sin_family = AF_INET;
-    address.sin_addr.S_un.S_addr = INADDR_ANY;
+    address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
 
     if (bind(listen_socket, (SOCKADDR *) &address, sizeof(address)) == SOCKET_ERROR) {
@@ -34,8 +108,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    printf("Waiting for a connection...\n");
-
     if (listen(listen_socket, SOMAXCONN) == SOCKET_ERROR) {
         printf("listen() failed: %d\n", WSAGetLastError());
         closesocket(listen_socket);
@@ -43,27 +115,24 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    SOCKET client_socket = accept(listen_socket, NULL, NULL);
-    if (client_socket == INVALID_SOCKET) {
-        printf("accept() failed: %d\n", WSAGetLastError());
-        closesocket(listen_socket);
-        WSACleanup();
-        return 1;
-    }
+    while (1) {
+        SOCKET client_socket = accept(listen_socket, NULL, NULL);
+        if (client_socket == INVALID_SOCKET) {
+            printf("accept() failed: %d\n", WSAGetLastError());
+            break;
+        }
 
-    int buffer_size = 512;
-    char buffer[buffer_size];
+        char buffer[1024] = {0};
+        recv(client_socket, buffer, sizeof(buffer) - 1, 0);
 
-    int bytes_received = recv(client_socket, buffer, buffer_size, 0);
-    if (bytes_received > 0) {
-        printf("Received: %.*s\n", bytes_received, buffer);
-        const char *response = "Hello from server!";
+        char *response = construct_html_page(directory);
         send(client_socket, response, (int) strlen(response), 0);
+        free(response);
+
+        closesocket(client_socket);
     }
 
-    closesocket(client_socket);
     closesocket(listen_socket);
     WSACleanup();
-
     return 0;
 }
